5a573e440cc3b9a741c2ce51dcc4e670bfcd857d
Teddy <Teddy@192.168.1.103>
2017-05-04 14:38:10 -0700

    removed Release folder

--- driverlib/Release/cpu.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-cpu.obj: C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/cpu.c
-cpu.obj: C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/cpu.h
-
-C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/cpu.c: 
-C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/cpu.h: 
--- driverlib/Release/utils.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-utils.obj: C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/utils.c
-utils.obj: C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/utils.h
-
-C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/utils.c: 
-C:/TI/CC3200SDK_1.3.0/cc3200-sdk/driverlib/utils.h: 
